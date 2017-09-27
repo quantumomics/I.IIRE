20fda0b56413068e4b97d2265997872135bbfb42
Quantumomics, Inc <31345894+quantumomics@users.noreply.github.com>
2017-09-27 11:36:37 +0530

    committed.

---
+++ index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+
+/*!
+ * -------
+ * I.IIRE®
+ * -------
+ * I.IIRE® — A Next Generation Intelligent Interactive Research Environment from Quantumømics, Inc."
+ * ___________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Quantumømics, Inc. [http://quantumomics.com/].
+ * @copyright : Sequømics, Inc. [http://sequomics.com/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 04-Sept-2018
+ * @purpose   : Imposing in to G®
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @build     : SEED™ — Skövde
+ *              └────── A Sequømics Product — http://seed.sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "I.IIRE®".
+ * ___________________________________________________________________________
+ */
+
+
+// # Usage: $ node -v
+// # Usage: $ npm -v
+// # Usage: $ grunt -version
